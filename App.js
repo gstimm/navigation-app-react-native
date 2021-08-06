5effd1a117e7322c5b34fda126f3c8e252befb26
gstimm <contato.gabrieltimm@gmail.com>
2021-08-06 15:40:35 -0300

    feat: finish 2nd class

--- App.js
+++ App.js
@@ -1,21 +1,129 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createDrawerNavigator } from '@react-navigation/drawer';
 
-export default function App() {
+import { AuthContext } from './context';
+
+import {
+  SignIn,
+  CreateAccount,
+  Profile,
+  Home,
+  Details,
+  Search,
+  Search2,
+  Splash,
+} from './Screens.js';
+
+const AuthStack = createStackNavigator();
+const Tabs = createBottomTabNavigator();
+const HomeStack = createStackNavigator();
+const SearchStack = createStackNavigator();
+const ProfileStack = createStackNavigator();
+const Drawer = createDrawerNavigator();
+const RootStack = createStackNavigator();
+
+const RootStackScreen = ({ userToken }) => (
+  <RootStack.Navigator headerMode='none'>
+    {userToken ? (
+      <RootStack.Screen name='App' component={DrawerStackScreen} />
+    ) : (
+      <RootStack.Screen name='Auth' component={AuthStackScreen} />
+    )}
+  </RootStack.Navigator>
+);
+
+const DrawerStackScreen = () => (
+  <Drawer.Navigator>
+    <Drawer.Screen name='Home' component={TabsScreen} />
+    <Drawer.Screen name='Profile' component={ProfileStackScreen} />
+  </Drawer.Navigator>
+);
+
+const AuthStackScreen = () => (
+  <AuthStack.Navigator>
+    <AuthStack.Screen
+      name='SignIn'
+      component={SignIn}
+      options={{ title: 'Sign In' }}
+    />
+    <AuthStack.Screen
+      name='CreateAccount'
+      component={CreateAccount}
+      options={{ title: 'Create Account' }}
+    />
+  </AuthStack.Navigator>
+);
+
+const HomeStackScreen = () => (
+  <HomeStack.Navigator>
+    <HomeStack.Screen name='Home' component={Home} />
+    <HomeStack.Screen
+      name='Details'
+      component={Details}
+      options={({ route }) => ({ title: route.params.name })}
+    />
+  </HomeStack.Navigator>
+);
+
+const SearchStackScreen = () => (
+  <SearchStack.Navigator>
+    <SearchStack.Screen name='Search' component={Search} />
+    <SearchStack.Screen name='Search2' component={Search2} />
+  </SearchStack.Navigator>
+);
+
+const ProfileStackScreen = () => (
+  <ProfileStack.Navigator>
+    <ProfileStack.Screen name='Profile' component={Profile} />
+  </ProfileStack.Navigator>
+);
+
+const TabsScreen = () => (
+  <Tabs.Navigator>
+    <Tabs.Screen name='Home' component={HomeStackScreen} />
+    <Tabs.Screen name='Search' component={SearchStackScreen} />
+  </Tabs.Navigator>
+);
+
+export default () => {
+  const [isLoading, setIsLoading] = React.useState(true);
+  const [userToken, setUserToken] = React.useState(null);
+
+  const authContext = React.useMemo(() => {
+    return {
+      signIn: () => {
+        setIsLoading(false);
+        setUserToken('1234');
+      },
+      signUp: () => {
+        setIsLoading(false);
+        setUserToken('1234');
+      },
+      signOut: () => {
+        setIsLoading(false);
+        setUserToken(null);
+      },
+    };
+  }, []);
+
+  React.useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 1000);
+  }, []);
+
+  if (isLoading) {
+    return <Splash />;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <AuthContext.Provider value={authContext}>
+      <NavigationContainer>
+        <RootStackScreen userToken={userToken} />
+      </NavigationContainer>
+    </AuthContext.Provider>
   );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+};
